baa44e3806d6cc2f31d907b348278d1a91fbca54
KayoBS <kayobs@hotmail.com>
2026-01-31 18:45:53 -0300

    Mudança no nome da pasta principal de 'lib' para 'src', reorganização da estrutura da pasta principal e adição do arquivo library.properties

---
+++ srd/Teremim-Standard-Library.h
@@ -0,0 +1,17 @@
+#ifndef TEREMIM_STANDARD_H_INCLUDED
+#define TEREMIM_STANDARD_H_INCLUDED
+
+// Teremim Standard Library - tstl
+// Adicionar o sensor ultrassônico
+
+#include "dec/Acelerometer.h"
+#include "dec/ColorSensor.h"
+#include "dec/EletronicComponent.h"
+#include "dec/InfraredSensor.h"
+#include "dec/Led.h"
+#include "dec/UltrasonicSensor.h"
+// #include "src/dec/SmartLed.h"
+#include "dec/Motor.h"
+#include "dec/MotorController.h"
+
+#endif
